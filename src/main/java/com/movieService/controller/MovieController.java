9136c62490897b8d586bfcfae0e344e26bf4a4a2
bp-stripa006 <stripathi1@xavient.com>
2016-12-28 19:05:53 +0530

    updated

---
+++ src/main/java/com/movieService/controller/MovieController.java
@@ -0,0 +1,31 @@
+package com.movieService.controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.movieService.types.LoginCrediantials;
+
+@RestController
+public class MovieController {
+
+	@RequestMapping(value = "/movie")
+	public ModelAndView moviePageView() {
+		return new ModelAndView("movie");
+	}
+	
+	
+	@RequestMapping(value = "/login")
+	public ModelAndView loginPageView() {
+		return new ModelAndView("login");
+	}
+	
+	@RequestMapping(value="/save",method = RequestMethod.POST)  
+    public ModelAndView saveUserDetails(@ModelAttribute("loginCrediantials") LoginCrediantials loginCrediantials){  
+        System.out.println(loginCrediantials.getUserName());
+        return new ModelAndView("success");
+    }  
+
+}
---
+++ src/main/java/com/movieService/types/LoginCrediantials.java
@@ -0,0 +1,20 @@
+package com.movieService.types;
+
+public class LoginCrediantials {
+
+	private String userName;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String password;
+
+}
