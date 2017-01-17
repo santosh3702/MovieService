5bdfff398b691b86859f0e5a0b200a49f411c7f6
bp-stripa006 <stripathi1@xavient.com>
2017-01-17 18:30:46 +0530

    updated

--- src/main/java/com/movieService/App.java
+++ src/main/java/com/movieService/App.java
@@ -2,11 +2,20 @@ package com.movieService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+
 
 
 @SpringBootApplication
-public class App {
+public class App /*extends SpringBootServletInitializer*/{
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+	
+	/*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }*/
 }
--- src/main/java/com/movieService/controller/MovieController.java
+++ src/main/java/com/movieService/controller/MovieController.java
@@ -1,27 +1,31 @@
 package com.movieService.controller;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.movieService.types.LoginCrediantials;
 
 @RestController
+@CrossOrigin()
+@RequestMapping(value = "/movieService")
 public class MovieController {
 
-	@RequestMapping(value = "/movie")
-	public ModelAndView moviePageView() {
-		return new ModelAndView("movie");
+	@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE ,consumes =  MediaType.APPLICATION_JSON_VALUE ,method = RequestMethod.POST)
+	public LoginCrediantials test(@RequestBody LoginCrediantials value) {
+		LoginCrediantials crediantials = new LoginCrediantials();
+		crediantials.setUserName(value.getUserName());
+		crediantials.setPassword(value.getPassword());
+		return crediantials;
 	}
 	
-
-	
-	@RequestMapping(value="/save",method = RequestMethod.POST)  
-    public ModelAndView saveUserDetails(@ModelAttribute("loginCrediantials") LoginCrediantials loginCrediantials){  
-        System.out.println(loginCrediantials.getUserName());
-        return new ModelAndView("success");
-    }  
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public boolean login(@RequestBody LoginCrediantials countryName)  {
+		
+		return true;
+	}
 
 }
